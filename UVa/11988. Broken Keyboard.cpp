46d3fb1a1a342313bc3732c87cf7a72a41c6e279
Jaimedev12 <A00833173@itesm.mx>
2022-04-25 11:26:53 -0500

    Create 11988. Broken Keyboard.cpp

---
+++ UVa/11988. Broken Keyboard.cpp
@@ -0,0 +1,11 @@
+// UVa 11988. Broken Keyboard.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
