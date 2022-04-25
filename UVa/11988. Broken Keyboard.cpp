0e6d89bff0537f91029ae96651a4958971303872
Jaimedev12 <A00833173@itesm.mx>
2022-04-25 15:07:19 -0500

    Update 11988. Broken Keyboard.cpp

--- UVa/11988. Broken Keyboard.cpp
+++ UVa/11988. Broken Keyboard.cpp
@@ -1,69 +1,39 @@
 // UVa 11988. Broken Keyboard
 
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-string s;
-
-class Node {
-public:
-    char Character;
-    Node* Next;
-};
-
-void insertAtTheFront(Node** head, char newChar){
-    // Preparar nuevo Nodo
-    Node* newNode = new Node();
-    newNode->Character = newChar; 
-    // Ponerlo en frente de la cabeza
-    newNode->Next = *head;
-    // Cambiar el valor de la cabeza
-    *head = newNode;
-}
-
-void insertAtTheBack(Node** head, char newChar){
-    
-    // Prepara nuevo Nodo
-    Node* newNode = new Node();
-    newNode->Character = newChar;
-    newNode->Next = NULL;
-    // Si la lista está vacía, esta será la cabeza
-    if (*head == NULL){
-        *head ==  newNode;
-        return;
-    }
-    // Encontrar último elemento
-    Node* last = *head;
-    while(last->Next != NULL){
-        last = last->Next;
-    }
-    // Agregar el puntero al último elemento
-    last->Next = newNode;
-}
-
-void printList(Node* node){
-    
-    while(node != NULL){
-        cout << node->Character;
-        node = node->Next;
-    }
-    
-}
-
 int main()
 {
-    cin >> s;
+    string s;
+    list<char> outputText;
+    list<char>::iterator it;
     
-    Node* head = new Node();
-    
-    for (int i = 0; i < s.length(); i++){
-        insertAtTheBack(&head, s[i]);
+    while (getline(cin, s)){
+        
+        outputText.clear();
+        it = outputText.begin();
+        
+        for (int i = 0; i < s.length(); i++){
+            
+            if (s[i] == '[')
+                it = outputText.begin();
+            
+            if (s[i] == ']')
+                it = outputText.end();
+                
+            if (s[i] != '[' && s[i] != ']'){
+                outputText.insert(it, s[i]);
+            }
+            
+        }
+        
+        for (it = outputText.begin(); it != outputText.end(); it++){
+            printf("%c", *it);
+        }
     }
-    
-    printList(head);
-    
+
     return 0;
 }
-
-// This_is_a_[Beiju]_text
