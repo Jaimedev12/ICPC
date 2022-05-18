5c980f989115b93a830abd2d671eaed06f190f2b
Jaimedev12 <A00833173@itesm.mx>
2022-05-17 19:21:05 -0500

    Create L_LeonelAndThePowersOfTwo.cpp

---
+++ ICPC 1/L_LeonelAndThePowersOfTwo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+typedef long long int lli;
+
+using namespace std;
+
+string s;
+
+string finalString(lli k){
+  if (k == 1){
+    s = "2";
+  } else if (k % 2 != 0){
+    s = "(2*" + finalString(k-1) + ")";
+  } else if(k % 2 == 0){
+    s = "(" + finalString(k/2) + ")^2";
+  }
+  return s;
+}
+
+int main() {
+  lli T, k;
+  cin >> T;
+
+  for(int i = 0; i < T; i++){
+    cin >> k;
+    cout << finalString(k) << endl;
+  }
+}
